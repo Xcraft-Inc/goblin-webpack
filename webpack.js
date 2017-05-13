96410eb57a7cc0aca9eece2daac318be27d6852d
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-05-13 17:12:10 +0200

    Use the new Xcraft module declaration

--- webpack.js
+++ webpack.js
@@ -1,17 +1,61 @@
 'use strict';
 
-const path = require ('path');
-const service = require ('./lib/service.js');
-
 /**
  * Retrieve the list of available commands.
  *
  * @returns {Object} The list and definitions of commands.
  */
 exports.xcraftCommands = function () {
-  const xUtils = require ('xcraft-core-utils');
   return {
-    handlers: service,
-    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json')),
+    handlers: require ('./lib/service.js'),
+    rc: {
+      pack: {
+        parallel: true,
+        desc: 'Pack a webapp',
+        options: {
+          params: {
+            required: 'location',
+          },
+        },
+      },
+      'server.start': {
+        parallel: true,
+        desc: 'Start a webpack server',
+        options: {
+          params: {
+            required: 'goblin',
+          },
+        },
+      },
+      'server.stop': {
+        parallel: true,
+        desc: 'Stop a webpack server',
+        options: {
+          params: {
+            required: 'goblin',
+          },
+        },
+      },
+      '_save.stats': {
+        parallel: true,
+        desc: null,
+        options: {
+          params: {
+            required: 'goblin',
+            optional: 'stats',
+          },
+        },
+      },
+      _request: {
+        parallel: true,
+        desc: null,
+        options: {
+          params: {
+            required: 'route',
+            optional: 'resp',
+          },
+        },
+      },
+    },
   };
 };
