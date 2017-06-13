b6d3c2c634022965b40026d410e46e24bbd7dc22
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-06-13 11:59:11 +0200

    Use the auto generated RC output
    Note that it's limited by shellcraft, for example, the cases
    where only an optional argument with or without array is needed
    is not supported.

--- webpack.js
+++ webpack.js
@@ -6,57 +6,5 @@
  * @returns {Object} The list and definitions of commands.
  */
 exports.xcraftCommands = function () {
-  return {
-    handlers: require ('./lib/service.js').handlers,
-    context: require ('./lib/service.js').context,
-    rc: {
-      pack: {
-        parallel: true,
-        desc: 'pack a webapp',
-        options: {
-          params: {
-            required: 'location',
-          },
-        },
-      },
-      'server.start': {
-        parallel: true,
-        desc: 'start a webpack server',
-        options: {
-          params: {
-            required: 'goblin',
-          },
-        },
-      },
-      'server.stop': {
-        parallel: true,
-        desc: 'stop a webpack server',
-        options: {
-          params: {
-            required: 'goblin',
-          },
-        },
-      },
-      '_save.stats': {
-        parallel: true,
-        desc: null,
-        options: {
-          params: {
-            required: 'goblin',
-            optional: 'stats',
-          },
-        },
-      },
-      _request: {
-        parallel: true,
-        desc: null,
-        options: {
-          params: {
-            required: 'route',
-            optional: 'resp',
-          },
-        },
-      },
-    },
-  };
+  return require ('./lib/service.js');
 };
