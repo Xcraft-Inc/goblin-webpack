f3027d59d1205596c6978410c0122310bbccb8a8
Mathieu Schroeter <mathieu@schroetersa.ch>
2020-12-14 08:35:08 +0100

    Fix release build with app-builder

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,24 @@
 const path = require('path');
 const xFs = require('xcraft-core-fs');
 
-const roots = xFs
-  .lsdir(path.join(__dirname, '..'), /^(xcraft|goblin)-/)
-  .map((root) => `../${root}/*`);
+let nodeModulesRoot = path.join(__dirname, '..');
+
+let roots = xFs
+  .lsdir(nodeModulesRoot, /^(xcraft-(core|contrib)|goblin)-/)
+  .map((root) => `${path.relative(__dirname, nodeModulesRoot)}/${root}/*`);
+
+if (process.env.GOBLIN_WEBPACK_RELEASEPATH) {
+  nodeModulesRoot = path.join(
+    process.env.GOBLIN_WEBPACK_RELEASEPATH,
+    'node_modules'
+  );
+
+  roots = roots.concat(
+    xFs
+      .lsdir(nodeModulesRoot, /^(xcraft-(core|contrib)|goblin)-/)
+      .map((root) => `${path.relative(__dirname, nodeModulesRoot)}/${root}/*`)
+  );
+}
 
 module.exports = {
   babelrcRoots: roots,
